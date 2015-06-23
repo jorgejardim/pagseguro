aca0eb77c506a796e611e6b5513a9ba349e23c53
Jorge Jardim <jorge@conteudodinamico.com.br>
2015-06-23 11:02:17 -0300

    correcao conflito cakephp

--- src/Controller/Component/PagseguroComponent.php
+++ src/Controller/Component/PagseguroComponent.php
@@ -9,50 +9,50 @@ class PagseguroComponent extends Component {
     const SEDEX = 2;
     const NOT_SPECIFIED = 3;
 
-    public $request;
+    public $request_pg;
     public $uid;
 
     public function initialize(array $config) {
         parent::initialize($config);
         $this->uid = uniqid();
-        $this->request = new \PagSeguroPaymentRequest;
-        $this->request->setShippingType(self::NOT_SPECIFIED);
-        $this->request->setReference($this->uid);
-        $this->request->setCurrency('BRL');
+        $this->request_pg = new \PagSeguroPaymentRequest;
+        $this->request_pg->setShippingType(self::NOT_SPECIFIED);
+        $this->request_pg->setReference($this->uid);
+        $this->request_pg->setCurrency('BRL');
     }
 
     public function add($id,$descricao=null,$quantidade=null,$custo=null,$peso=null,$custo_envio=null){
-        $this->request->addItem($id,$descricao,$quantidade,$custo,$peso,$custo_envio);
+        $this->request_pg->addItem($id,$descricao,$quantidade,$custo,$peso,$custo_envio);
     }
 
     public function setReference($reference){
-        $this->request->setReference($reference);
+        $this->request_pg->setReference($reference);
     }
 
     public function setShippingType($type){
-        $this->request->setShippingType($type);
+        $this->request_pg->setShippingType($type);
     }
 
     public function setSender($nome,$email=null,$area=null,$telefone=null,$tipo_documento=null,$documento=null){
-        $this->request->setSender($nome, $email, $area, $telefone, $tipo_documento, $documento);
+        $this->request_pg->setSender($nome, $email, $area, $telefone, $tipo_documento, $documento);
     }
 
     public function setShippingAddress($cep,$logradouro=null,$numero=null,$complemento=null,$bairro=null,$cidade=null,$estado=null,$pais=null){
-        $this->request->setShippingAddress($cep,$logradouro,$numero,$complemento,$bairro,$cidade,$estado,$pais);
+        $this->request_pg->setShippingAddress($cep,$logradouro,$numero,$complemento,$bairro,$cidade,$estado,$pais);
     }
 
     public function redirectURL($url){
         $url = Router::url($url, true);
-        $this->request->setRedirectUrl($url);
+        $this->request_pg->setRedirectUrl($url);
     }
 
     public function notificationURL($url){
         $url = Router::url($url, true);
-        $this->request->addParameter('notificationURL', $url);
+        $this->request_pg->addParameter('notificationURL', $url);
     }
 
     public function addParameter($key, $value){
-        $this->request->addParameter($key, $value);
+        $this->request_pg->addParameter($key, $value);
     }
 
     public function register($email=null,$token=null,$environment=null){
@@ -63,6 +63,18 @@ class PagseguroComponent extends Component {
 
         $config = new \PagSeguroAccountCredentials($email, $token);
         \PagSeguroConfig::setEnvironment($environment?$environment:'production');
-        return $this->request->register($config);
+        return $this->request_pg->register($config);
+    }
+
+    public function checkTransaction($notificationCode,$email=null,$token=null){
+
+        $email = $email?$email:$this->config('email');
+        $token = $token?$token:$this->config('token');
+
+        $config = new \PagSeguroAccountCredentials($email, $token);
+        return \PagSeguroNotificationService::checkTransaction(
+            $config,
+            $notificationCode
+        );
     }
 }
